c5207f20492827ba6d8c3aa4490f78faee137d19
NitikaSheoran <nitikasheoran5@gmail.com>
2024-08-29 22:32:18 +0530

    day 4 of learning react. (understood how react renders the functions and made a customised react folder to understand its working)

--- vitereact01/src/App.jsx
+++ vitereact01/src/App.jsx
@@ -1,9 +1,10 @@
 import Day3 from "./Day3"
 
 function App() {
+  const username = "chai aur code";
   return (
     <>
-    <h1>Hello world</h1>
+    <h1>Hello world ({username})</h1>   
     <Day3 />
     <p>used the concept of fragments</p>
     </>
--- vitereact01/src/main.jsx
+++ vitereact01/src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function App2(){
+  return(
+    <div>
+      <h1>Day 4</h1>
+    </div>
+  )
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// This object might give an error because the syntax or naming for keys might not be correct.
+// The HTML provided by the component is converted to this tree-like structure in React.
+const customElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google',
+}
+
+const user = "chai aur code";
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'click me to visit google',
+  user
+)
+createRoot(document.getElementById('root'))
+.render(
+  <>
+  <App />     {/*App() also works because at the end app is a function returning some html element */}
+  <App2 />   
+  {/* customElement won't work directly because React doesn't understand direct HTML or manually created objects unless they are properly formatted.*/}
+  {reactElement}   {/*// Rendered using React.createElement, which is how React internally handles JSX.*/}
+  </>
 )
